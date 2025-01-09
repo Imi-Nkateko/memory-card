5ebaad26e36538619a56937d072b30e66d3f6537
Imi Nkateko <shikuhelenkateko@gmail.com>
2025-01-10 00:26:33 +0200

    install and setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+
+
+const Header = () => {
+  return (
+    
+    <header>
+        <div className="logo">
+            Logo
+        </div>
+        <div className="score-board">
+            <h1>Score: 0</h1>
+            <h3>High Score</h3>
+        </div>
+    </header>
+  )
+}
+
+export default Header
