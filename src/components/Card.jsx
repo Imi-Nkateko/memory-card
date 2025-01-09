d6c035e48f9aa69d0e0f40b057fdf050231d50dd
Imi Nkateko <shikuhelenkateko@gmail.com>
2025-01-10 01:23:50 +0200

    import Styles.css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,14 @@
+import "./Style.css";
+
 const Card = ({ cardData }) => {
+	function handleClick() {
+		alert("score + 1");
+	}
+
 	return (
-		<div className="card-container">
+		<div className="card" onClick={handleClick}>
 			<div className="card-image">
-				<img src={cardData.url} alt="" />
+				<img src={cardData.url} alt="Card Image" />
 			</div>
 			<h2>{cardData.title}</h2>
 		</div>
