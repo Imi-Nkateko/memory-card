4052059eee6dcca661f4da64e75eb5f0336371fa
Imi Nkateko <shikuhelenkateko@gmail.com>
2025-01-16 19:53:00 +0200

    updating high score

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import "./Style.css";
 import logo from "./assets/courage-the-cowardly-dog.svg";
+import { useState, useEffect } from "react";
 
 const Header = ({ points }) => {
+	const [highScore, setHighScore] = useState(0);
 
-
-
-
+	useEffect(() => {
+		setHighScore((prevHighScore) => Math.max(prevHighScore, points));
 	
+	}, [points]);
+
 	return (
 		<header>
 			<div className="logo">
@@ -14,6 +17,7 @@ const Header = ({ points }) => {
 			</div>
 			<div className="score-board">
 				<h1 className="score">Score: {points}</h1>
+				<h3>High Score: {highScore}</h3>
 			</div>
 		</header>
 	);
